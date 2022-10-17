50869b3360e960f8b99561ac0df1fd6939c56841
Ujjwal Garg <113996919+Ujjwal03@users.noreply.github.com>
2022-10-17 20:49:11 +0530

    Create plus.py

---
+++ plus.py
@@ -0,0 +1,32 @@
+mport math
+import os
+import random
+import re
+import sys
+
+
+def plusMinus(arr):
+    n=len(arr)
+    negco=0
+    poco=0
+    zer=0
+    for i in arr:
+        if(i<0):
+            negco+=1
+        elif(i>0):
+            poco+=1
+        else:
+            zer+=1
+    negpro=negco/n
+    popro=poco/n
+    zepro=zer/n
+    print(popro)
+    print(negpro)
+    print(zepro)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
